42256a4a97b782998d8d80dba5179e0b05b5a9d9
Jason Cluley <jaxeus@gmail.com>
2023-11-21 13:36:29 +1300

    Added notes.js

---
+++ notes.js
@@ -0,0 +1,17 @@
+/* 
+Getting started with React and Next.js
+Add the following scripts to index.html:
+<script src="https://unpkg.com/react@17/umd/react.development.js"></script>
+<script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
+react dom allows you to use less code to manipulate the DOM
+<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+babel will compile JSX to JS
+
+for example
+<script type="text/jsx">
+      const app = document.getElementById('app');
+      ReactDOM.render(<h1>Develop. Preview. Ship. 🚀</h1>, app);
+</script>
+
+---------------------
+*/
